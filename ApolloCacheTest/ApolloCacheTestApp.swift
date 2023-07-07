d59be4aabeed2c20e908a09f63fafcfcc4d7ad7a
Aleksander Lorenc <aleksander.lorenc@lifeworks.com>
2023-07-07 07:57:04 +0200

    Initial Commit

---
+++ ApolloCacheTest/ApolloCacheTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApolloCacheTestApp.swift
+//  ApolloCacheTest
+//
+//  Created by Aleksander Lorenc on 07/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ApolloCacheTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
